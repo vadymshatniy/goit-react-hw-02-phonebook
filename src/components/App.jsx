7caace2a64199d95671ab50cd30d34778160b457
Vadym Shatnii <vadymshatniy@gmail.com>
2023-06-14 23:35:01 +0300

    commit 14

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,42 +8,42 @@ import profileStyles from "./App.module.css"
 
 class App extends Component {
   state = {
-  contacts: [],
-  filter: '',
+    contacts: [],
+    filter: '',
   }
 
-  // checkDuplication = data => {
-  //   this.state.contacts.map(contact => {
-  //     if(contact.name === data.name) {
-  //       alert("is already in contacts")
-  //     };
-  // })
-  
-  formSubmitHandler = data => {
-    this.setState({ contacts: [...this.state.contacts, data]});
-    this.setState({ name: data.name });
-  };
+  checkDuplication = data => {
+      if([this.state.contacts].some(contact => contact.name === data.name)) {
+       alert(`${data.name} is already in contacts`)
+      } else {
+        this.setState({ contacts: [...this.state.contacts, data] })
+      }
+    }
+      
+    formSubmitHandler = data => {
+      this.checkDuplication(data);
+    }
     
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  }
+    changeFilter = e => {
+      this.setState({ filter: e.currentTarget.value });
+    }
 
-  deleteContact = (id) => {
-        this.setState(prevState => ({contacts: prevState.contacts.filter(contact => contact.id !== id), }));
+    deleteContact = (id) => {
+      this.setState(prevState => ({ contacts: prevState.contacts.filter(contact => contact.id !== id), }));
     }
   
-  render() {
-    const loweredFilter = this.state.filter.toLowerCase();
-    const filteredContact = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(loweredFilter));
-    return (
-      <div className={profileStyles.container}>
-        <Form onSubmit={this.formSubmitHandler} />
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.changeFilter} />
-        <ContactList contacts={filteredContact} onDeleteContact={this.deleteContact} />
-      </div >
-    )
-  }
+    render() {
+      const loweredFilter = this.state.filter.toLowerCase();
+      const filteredContact = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(loweredFilter));
+      return (
+        <div className={profileStyles.container}>
+          <Form onSubmit={this.formSubmitHandler} />
+          <h2>Contacts</h2>
+          <Filter value={this.state.filter} onChange={this.changeFilter} />
+          <ContactList contacts={filteredContact} onDeleteContact={this.deleteContact} />
+        </div >
+      )
+    }
 };
 
 export default App;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ContactItem from '../ContactItem/ContactItem';
 
-function ContactList({ contacts }) {
+function ContactList({ contacts, onDeleteContact }) {
     return (
         <ul>
             {contacts.map(contact =>
-                <ContactItem key={contact.id} id={contact.id} name={contact.name} number={contact.number} />)}
+                <ContactItem
+                    key={contact.id}
+                    id={contact.id}
+                    name={contact.name}
+                    number={contact.number}
+                    onDeleteContact={onDeleteContact}
+                />)}
         </ul>
     )
 };
